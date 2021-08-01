6090066bf045b2c51a8df70537c737dec3e51fc7
krishnasamal2397 <krishna.samal@publicissapient.com>
2021-08-01 16:54:32 +0530

    remove unwanted files

--- src/Store/saga.js
+++
@@ -1,7 +0,0 @@
-import { all, fork } from "redux-saga/effects";
-import watchuserquery from "../Action/Apicall";
-
-export default function* watchersrootSaga() {
-  
-  yield all([fork(watchuserquery)]);
-}
